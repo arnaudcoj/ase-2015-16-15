1608bd0b111719be31833c945827cbf394b5c1aa
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-12-15 17:20:11 +0100

    correction from tp8

--- charneux_cojez_tp9_ase/inode.c
+++ charneux_cojez_tp9_ase/inode.c
@@ -2,81 +2,68 @@
 #include "drive.h"
 
 void read_inode (unsigned int inumber, struct inode_s *inode){
-    read_bloc_n(current_volume, inumber, (unsigned char *) inode, sizeof(struct inode_s));
+  read_bloc_n(current_volume, inumber, (unsigned char *) inode, sizeof(struct inode_s));
 }
 void write_inode (unsigned int inumber, const struct inode_s *inode){
-    write_bloc_n(current_volume, inumber, (unsigned char *) inode, sizeof(struct inode_s));
+  write_bloc_n(current_volume, inumber, (unsigned char *) inode, sizeof(struct inode_s));
 }
 
 unsigned int create_inode(enum file_type_e type){
-    unsigned inumber, i;
-    struct inode_s inode;
-    inode.inode_type = type;
-    inode.inode_size = 0;
-    for(i = 0; i<NBDIRECT; i++)
-        inode.inode_direct[i] = 0;
-    inode.inode_indirect = 0;
-    inode.inode_2Xindirect = 0;
-    inumber = new_bloc();
-    if(inumber == 0)
-        return 0;
-    write_inode(inumber, &inode);
-    return inumber;
+  unsigned inumber, i;
+  struct inode_s inode;
+  inode.inode_type = type;
+  inode.inode_size = 0;
+  for(i = 0; i<NBDIRECT; i++)
+    inode.inode_direct[i] = 0;
+  inode.inode_indirect = 0;
+  inumber = new_bloc();
+  if(inumber == 0)
+    return 0;
+  write_inode(inumber, &inode);
+  return inumber;
 } 
 
 void free_blocs(unsigned table[], unsigned size){
-    int i;
-    for(i=0; i<size; i++)
-      if(table[i] != 0)
-        free_bloc(table[i]);
+  int i;
+  for(i=0; i<size; i++)
+    if(table[i] != 0)
+      free_bloc(table[i]);
 }
 
 int delete_inode(unsigned int inumber){
-    struct inode_s inode;
-     unsigned bloc[NBBLOCPARBLOC], bbloc[NBBLOCPARBLOC];
-       unsigned i;
-    read_inode(inumber, &inode);
-    free_bloc(inumber);
-    free_blocs(inode.inode_direct, NBDIRECT);
-    if(inode.inode_indirect != 0){
-        read_bloc_n(current_volume, inode.inode_indirect, (unsigned char *) &bloc, NBBLOCPARBLOC * sizeof(unsigned));        
-        free_blocs(bloc, NBBLOCPARBLOC);
-        free_bloc(inode.inode_indirect);
-    }
-    
-    if(inode.inode_2Xindirect != 0){
-        read_bloc_n(current_volume, inode.inode_2Xindirect, (unsigned char *) &bbloc, NBBLOCPARBLOC * sizeof(unsigned));
-        for(i=0; i<NBBLOCPARBLOC; i++){
-            if(bbloc[i] != 0){
-                read_bloc_n(current_volume, bbloc[i], (unsigned char *) &bloc, NBBLOCPARBLOC * sizeof(unsigned));        
-                free_blocs(bloc, NBBLOCPARBLOC);
-                free_bloc(bbloc[i]);
-            } 
-        }
-        free_bloc(inode.inode_2Xindirect);
-	}
-    return EXIT_SUCCESS;
+  struct inode_s inode;
+  unsigned bloc[NBBLOCPARBLOC];
+  read_inode(inumber, &inode);
+  free_bloc(inumber);
+  free_blocs(inode.inode_direct, NBDIRECT);
+  if(inode.inode_indirect != 0){
+    read_bloc_n(current_volume, inode.inode_indirect, (unsigned char *) &bloc, NBBLOCPARBLOC * sizeof(unsigned));        
+    free_blocs(bloc, NBBLOCPARBLOC);
+    free_bloc(inode.inode_indirect);
+  }
+   
+  return EXIT_SUCCESS;
 }
 
 unsigned int vbloc_of_fbloc(unsigned int inumber, unsigned int fbloc, bool_t do_allocate){
-    struct inode_s inode;
-    unsigned bloc[NBBLOCPARBLOC], bbloc[NBBLOCPARBLOC], n, nn;
-    read_inode(inumber, &inode);
-    if(fbloc < NBDIRECT){
-      if(do_allocate){
-      	if(inode.inode_direct[fbloc] == 0){
-	        inode.inode_direct[fbloc] = new_bloc_zero();
-      	  write_inode(inumber, &inode);
-	      }
+  struct inode_s inode;
+  unsigned bloc[NBBLOCPARBLOC];
+  read_inode(inumber, &inode);
+  if(fbloc < NBDIRECT){
+    if(do_allocate){
+      if(inode.inode_direct[fbloc] == 0){
+	inode.inode_direct[fbloc] = new_bloc_zero();
+	write_inode(inumber, &inode);
       }
-      return inode.inode_direct[fbloc];
     }
+    return inode.inode_direct[fbloc];
+  }
   fbloc -= NBDIRECT;
   if(fbloc < NBBLOCPARBLOC){
     if(inode.inode_indirect == 0){
       if(do_allocate){
-          inode.inode_indirect = new_bloc_zero();
-          write_inode(inumber, &inode);
+	inode.inode_indirect = new_bloc_zero();
+	write_inode(inumber, &inode);
       }
     }
     
@@ -89,33 +76,7 @@ unsigned int vbloc_of_fbloc(unsigned int inumber, unsigned int fbloc, bool_t do_
     }
     return bloc[fbloc];
   }   
-  
-  
-  fbloc -= NBBLOCPARBLOC;
-  if(fbloc < NBBLOCPARBLOC * NBBLOCPARBLOC){
-    if(inode.inode_2Xindirect == 0){
-      if(do_allocate){
-          inode.inode_2Xindirect = new_bloc_zero();
-          write_inode(inumber, &inode);
-      }
-    }
-    read_bloc_n(current_volume, inode.inode_2Xindirect,(unsigned char *)&bloc, NBBLOCPARBLOC * sizeof(unsigned));
-    n = fbloc / NBBLOCPARBLOC;
-    if(bloc[n] == 0){
-       bloc[n] = new_bloc_zero();
-       write_bloc_n(current_volume, inode.inode_2Xindirect,(unsigned char *) &bloc, NBBLOCPARBLOC * sizeof(unsigned));
-    }
-    read_bloc_n(current_volume, bloc[fbloc],(unsigned char *)&bbloc, NBBLOCPARBLOC * sizeof(unsigned));
-    nn = fbloc % NBBLOCPARBLOC;
-    if(bbloc[nn] == 0){
-      bbloc[nn] = new_bloc_zero();
-      write_bloc_n(current_volume, bloc[n],(unsigned char *) &bbloc, NBBLOCPARBLOC * sizeof(unsigned));
-    }
-    return bbloc[nn];
-  }
     
-    
-    
-    printf("hors limite\n");
-    exit(EXIT_FAILURE);
+  printf("hors limite\n");
+  exit(EXIT_FAILURE);
 }
