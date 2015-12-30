b8129453a8d71221e8a9ccfb179fa5862240c4e3
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-12-30 23:36:12 +0100

    tp9 finished

--- charneux_cojez_tp9_ase/mi_user.h
+++ charneux_cojez_tp9_ase/mi_user.h
@@ -2,7 +2,13 @@
 #define MI_USER_H
 
 #include "config_hardware.h"
+#include <hardware.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mi_syscall.h"
 
 int sum(void *ptr);
+void init_user(void);
 
 #endif //MI_USER_H
